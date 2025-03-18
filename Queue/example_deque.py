1210406f641cdb6dfa1049037434ba2472db790e
muhammed-anees-pp <muhammedanees.work@gmail.com>
2025-03-18 20:34:36 +0530

    queue: problems

---
+++ Queue/example_deque.py
@@ -0,0 +1,45 @@
+from collections import deque
+
+class Queue:
+    def __init__(self):
+        self.queue = deque()
+    
+    # Add item to the back of the queue
+    def enqueue(self, item):
+        self.queue.append(item)
+    
+    # Remove item from the front of the queue
+    def dequeue(self):
+        if len(self.queue) > 0:
+            return self.queue.popleft()
+        else:
+            return "Queue is empty"
+    
+    # View the front element without removing it
+    def peek(self):
+        if len(self.queue) > 0:
+            return self.queue[0]
+        else:
+            return "Queue is empty"
+    
+    # Check if the queue is empty
+    def is_empty(self):
+        return len(self.queue) == 0
+    
+    # Get the size of the queue
+    def size(self):
+        return len(self.queue)
+        
+    # Display
+    def display(self):
+        return print("Queue: ", self.queue)
+
+# Example test case          
+q = Queue()
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+q.display()
+print(q.dequeue())
+q.display()
+
---
+++ Queue/example_ll.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+    
+class Queue:
+    def __init__(self):
+        self.start = self.end = None
+    
+    def enqueue(self, data):
+        new_node = Node(data)
+        if not self.end:
+            self.start = self.end = new_node
+            return
+        self.end.next = new_node
+        self.end = new_node
+    
+    def dequeue(self):
+        if not self.start:
+            return "Queue is empty"
+        dequeued_data = self.start.data
+        self.start = self.start.next
+        if not self.start:
+            self.end = None
+        return dequeued_data
+    
+    def display(self):
+        temp = self.start
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+
+# Example test case
+q = Queue()
+q.enqueue(10)
+q.enqueue(20)
+q.enqueue(30)
+q.display()  
+print("Dequeued:", q.dequeue())  
+q.display()
