abd1a1e978f136ba5037cd757d267a88e24b09e6
muhammed-anees-pp <muhammedanees.work@gmail.com>
2025-04-29 18:35:41 +0530

    heap: problems

--- Heap/heap_sort_dsc.py
+++ Heap/heap_sort_dsc.py
@@ -22,6 +22,15 @@ def heap_sort(arr):
         arr[i],arr[0] = arr[0],arr[i]
         heapify(arr,i,0)
     print(arr)
+
+    # For print in ASC
+    start = 0
+    end = n-1
+    while start < end:
+        arr[start],arr[end] = arr[end],arr[start]
+        start += 1
+        end -= 1
+    print(arr)
         
         
 arr = [70, 50, 60, 30, 40, 20]
